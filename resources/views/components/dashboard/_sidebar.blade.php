678b1b0b227fa81a8523cbefb4952f7d4168bdba
kalinggapadelmuhamad <enginerbros@gmail.com>
2024-01-24 23:35:07 +0700

    Update Dashboard Page

--- resources/views/components/dashboard/_sidebar.blade.php
+++ resources/views/components/dashboard/_sidebar.blade.php
@@ -16,99 +16,63 @@
     </li>
 
     <hr class="sidebar-divider">
+    @if (Auth::user()->role != 'User')
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Master Data
+        </div>
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Master Data
-    </div>
-
-    <li class="nav-item {{ $page === 'alternatif' ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('alternatif.index') }}">
-            <i class="fas fa-fw fa-users"></i>
-            <span>Alternatif</span>
-        </a>
-    </li>
-
-    <li class="nav-item {{ $page === 'kriteria' ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('kriteria.index') }}">
-            <i class="fas fa-fw fa-poll-h"></i>
-            <span>Kriteria</span>
-        </a>
-    </li>
-
-    <li class="nav-item {{ $page === 'nilai' ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('penilaian.index') }}">
-            <i class="fas  fa-fw fa-file-invoice"></i>
-            <span>Nilai</span>
-        </a>
-    </li>
-
-    <li class="nav-item {{ $page === 'metode' ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('metode.index') }}">
-            <i class="fas fa-fw fa-spinner"></i>
-            <span>Metode</span>
-        </a>
-    </li>
-
-    <li class="nav-item {{ $page === 'laporan' ? 'active' : '' }}">
-        <a class="nav-link" href="">
-            <i class="fas fa-fw fa-file-pdf"></i>
-            <span>Laporan</span>
-        </a>
-    </li>
-
-    {{-- @if (Auth::user()->role == 'admin')
-        <li class="nav-item {{ $page === 'kriteria' ? 'active' : '' }}">
-            <a class="nav-link" href="{{ route('indexKriteria') }}">
-                <i class="fas fa-fw fa-folder"></i>
-                <span>Data Kriteria</span></a>
+        <li class="nav-item {{ $page === 'alternatif' ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('alternatif.index') }}">
+                <i class="fas fa-fw fa-users"></i>
+                <span>Alternatif</span>
+            </a>
         </li>
 
-        <li class="nav-item {{ $page === 'subkriteria' ? 'active' : '' }}">
-            <a class="nav-link" href="{{ route('indexSubKriteria') }}">
-                <i class="fas fa-fw fa-folder-open"></i>
-                <span>Data Sub Kriteria</span></a>
+        <li class="nav-item {{ $page === 'kriteria' ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('kriteria.index') }}">
+                <i class="fas fa-fw fa-poll-h"></i>
+                <span>Kriteria</span>
+            </a>
+        </li>
+
+        <li class="nav-item {{ $page === 'nilai' ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('penilaian.index') }}">
+                <i class="fas  fa-fw fa-file-invoice"></i>
+                <span>Nilai</span>
+            </a>
+        </li>
+
+        <li class="nav-item {{ $page === 'metode' ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('metode.index') }}">
+                <i class="fas fa-fw fa-spinner"></i>
+                <span>Metode</span>
+            </a>
+        </li>
+
+        <li class="nav-item {{ $page === 'laporan' ? 'active' : '' }}">
+            <a class="nav-link" href="">
+                <i class="fas fa-fw fa-file-pdf"></i>
+                <span>Laporan</span>
+            </a>
+        </li>
+
+
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Master User
+        </div>
+
+        <li class="nav-item {{ $page === 'user' ? 'active' : '' }}">
+            <a class="nav-link" href="">
+                <i class="fas fa-fw fa-users-cog"></i>
+                <span>User</span>
+            </a>
         </li>
     @endif
-
-    <li class="nav-item {{ $page === 'alternatif' ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('indexDataAlternatif') }}">
-            <i class="fas fa-fw fa-users"></i>
-            <span>Data Alternatif</span></a>
-    </li>
-
-    <li class="nav-item {{ $page === 'penilaian' ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('indexPenilaian') }}">
-            <i class="fas fa-fw fa-edit"></i>
-            <span>Data Penilaian</span></a>
-    </li>
-
-    <li class="nav-item {{ $page === 'perhitungan' ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('indexPerhitungan') }}">
-            <i class="fas fa-fw fa-calculator"></i>
-            <span>Data Perhitungan</span></a>
-    </li>
-
-    <li class="nav-item {{ $page === 'hasil' ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('indexHasil') }}">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>Data Hasil Akhir</span></a>
-    </li> --}}
-
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Master User
-    </div>
-
-    <li class="nav-item {{ $page === 'user' ? 'active' : '' }}">
-        <a class="nav-link" href="">
-            <i class="fas fa-fw fa-users-cog"></i>
-            <span>User</span>
-        </a>
-    </li>
 
     <li class="nav-item {{ $page === 'profile' ? 'active' : '' }}">
         <a class="nav-link" href="">
--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -6,116 +6,27 @@
     <div class="mb-4">
 
         <div class="row">
-            @if (Auth::user()->role == 'admin')
-                <div class="col-xl-4 col-md-6 mb-4">
-                    <div class="card border-left-info shadow h-100 py-2">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    {{-- <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="{{ route('indexKriteria') }}"
-                                            class="text-secondary text-decoration-none">Data Kriteria</a></div>
-                                </div> --}}
-                                    <div class="col-auto">
-                                        <i class="fas fa-folder fa-2x text-gray-300"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-xl-4 col-md-6 mb-4">
-                        <div class="card border-left-secondary shadow h-100 py-2">
-                            <div class="card-body">
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col mr-2">
-                                        {{-- <div class="h5 mb-0 font-weight-bold text-gray-800"><a
-                                            href="{{ route('indexSubKriteria') }}"
-                                            class="text-secondary text-decoration-none">Data Sub Kriteria</a></div>
-                                </div> --}}
-                                        <div class="col-auto">
-                                            <i class="fas fa-folder-open fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+            @if (Auth::user()->role != 'User')
+                <div class="container d-flex justify-content-center align-self-center p-5">
+                    <h3 class="font-weight-bold text-center">Selamat Datang {{ Auth::user()->name }} Di Aplikasi PILMAPRES
+                        IIB Darmajaya
+                        Menggunakan
+                        Metode Profile Matching & Bubble Sort
+                    </h3>
+                </div>
+            @else
+                <div class="container d-flex justify-content-center align-self-center p-5">
+                    <h4 class="font-weight-bold text-center">Selamat Datang {{ Auth::user()->name }} Di Aplikasi PILMAPRES
+                        IIB Darmajaya
+                        <br>
+                        @if (empty(Auth::user()->alternatif->semester1))
+                            Harap Segera lengkapi persyaratan di halaman <a href="">profile</a>
+                        @else
+                            Untuk informasi lebih lanjut silahkan kunjungi instagram <a href="">KMH Darmajaya</a>
+                        @endif
+                    </h4>
+                </div>
             @endif
-
-            <div class="col-xl-4 col-md-6 mb-4">
-                <div class="card border-left-danger shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                {{-- <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                    <a href="{{ route('indexDataAlternatif') }}"
-                                        class="text-secondary text-decoration-none">Data Alternatif</a>
-                                </div> --}}
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-users fa-2x text-gray-300"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-xl-4 col-md-6 mb-4">
-                <div class="card border-left-success shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                    {{-- <a href="{{ route('indexPenilaian') }}" class="text-secondary text-decoration-none">Data
-                                        Penilaian --}}
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-edit fa-2x text-gray-300"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-xl-4 col-md-6 mb-4">
-                <div class="card border-left-primary shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                    {{-- <a href="{{ route('indexPerhitungan') }}"
-                                        class="text-secondary text-decoration-none">Data Perhitungan
-                                    </a> --}}
-                                </div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-calculator fa-2x text-gray-300"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="col-xl-4 col-md-6 mb-4">
-                <div class="card border-left-warning shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                    {{-- <a href="{{ route('indexHasil') }}" class="text-secondary text-decoration-none">Data
-                                        Hasil Akhir
-                                    </a> --}}
-                                </div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-chart-area fa-2x text-gray-300"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 @endsection
